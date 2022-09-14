37321b67fe9e48e14ad4200fd989b6193db154df
linhvu <linh.tp.vu@gmail.com>
2022-09-15 01:27:17 +1000

    Added AddSalesViewModel

--- MemberManagementSystem/MemberManagementSystem/ViewModel/AddSalesViewModel.cs
+++ MemberManagementSystem/MemberManagementSystem/ViewModel/AddSalesViewModel.cs
@@ -1,6 +1,62 @@
-﻿namespace MemberManagementSystem.ViewModel
+﻿using MemberManagementSystem.Commands;
+using MemberManagementSystem.Model;
+using MemberManagementSystem.Service;
+using System;
+using System.Windows.Input;
+
+namespace MemberManagementSystem.ViewModel
 {
-    internal class AddSalesViewModel : ViewModelBase
+    internal class AddProductViewModel : ViewModelBase
     {
+        private string _productID;
+
+        public string ProductID
+        {
+            get { return _productID; }
+            set { _productID = value; OnPropertyChanged(nameof(ProductID)); }
+        }
+
+        private string _memberID;
+
+        public string MemberID
+        {
+            get { return _memberID; }
+            set { _memberID = value; OnPropertyChanged(nameof(MemberID)); }
+        }
+
+        private string _dateTime;
+
+        public string DateTime
+        {
+            get { return _dateTime; }
+            set { _dateTime = value; OnPropertyChanged(nameof(DateTime)); }
+        }
+
+        private int _quantity;
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; OnPropertyChanged(nameof(Quantity)); }
+        }
+
+
+
+        public AddProductViewModel(NavigateService navService, Book<Sales> salesBook)
+        {
+            HomePage = new NavigateCommand(navService, nameof(HomeViewModel));
+            SubmitProduct = new AddRecordCommand<Sales>(salesBook, CreateSales);
+        }
+
+
+
+        public ICommand HomePage { get; }
+        public ICommand SubmitSales { get; }
+
+        private Product CreateSales()
+        {
+            int quantity = int.Parse(Quantity);
+            return new Product(App.ID, ProductID, MemberID, DateTime, quantity);
+        }
     }
 }

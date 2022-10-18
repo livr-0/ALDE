19d4a6d098eb5ef1dda244585b51b19d577478a4
Andy Pham <andypham1303@gmail.com>
2022-10-19 09:19:30 +1100

    ok

--- MemberManagementSystem/MemberManagementSystem/obj/Debug/net6.0-windows/View/ViewMemberView.g.i.cs
+++ MemberManagementSystem/MemberManagementSystem/obj/Debug/net6.0-windows/View/ViewMemberView.g.i.cs
@@ -47,13 +47,13 @@ namespace MemberManagementSystem.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MemberManagementSystem;component/view/viewmemberview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MemberManagementSystem;V1.0.0.0;component/view/viewmemberview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\ViewMemberView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace MemberManagementSystem.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]

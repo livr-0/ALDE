acc293f43b0b0454c70e4adf81f7ad9f645afb93
LINH VU <103519240@student.swin.edu.au>
2022-10-18 19:52:32 +1100

    added user manual access on home page

---
+++ MemberManagementSystem/MemberManagementSystem/Commands/OpenURLCommand.cs
@@ -0,0 +1,24 @@
+﻿using MemberManagementSystem.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MemberManagementSystem.Commands
+{
+    internal class OpenURLCommand : CommandBase
+    {
+        private string _url;
+        public OpenURLCommand(string url)
+        {
+            _url = url;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Process.Start(new ProcessStartInfo { FileName = _url, UseShellExecute = true });
+        }
+    }
+}
--- MemberManagementSystem/MemberManagementSystem/ViewModel/HomeViewModel.cs
+++ MemberManagementSystem/MemberManagementSystem/ViewModel/HomeViewModel.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Navigation;
 using MemberManagementSystem.Commands;
 using MemberManagementSystem.Service;
+using static System.Net.WebRequestMethods;
 
 namespace MemberManagementSystem.ViewModel
 {
     internal class HomeViewModel : ViewModelBase
     {
+        private string _userManualURL;
 
+        public string UserManualURL
+        {
+            get => _userManualURL;
+        }
+        public ICommand OpenUserManual { get; }
         public ICommand AddMemberPage{ get; }
         public ICommand AddSalesPage { get; }
         public ICommand AddProductPage { get; }
@@ -20,9 +29,9 @@ namespace MemberManagementSystem.ViewModel
         public ICommand ViewSalesPage { get; }
         public ICommand ViewProductPage { get; }
         public ICommand ViewUserPage { get; }
-
         public HomeViewModel(NavigateService navService)
         {
+            _userManualURL = "http://docs.google.com/document/d/e/2PACX-1vRYjZd2a218nfu8J-17-BgB_KSo5vXImL36_Yuk0j0LsChp7y5l2x1z-CxWExTkM1Q3f7x2tzNydnlM/pub";
             AddMemberPage = new NavigateCommand(navService, nameof(AddMemberViewModel));
             AddSalesPage = new NavigateCommand(navService, nameof(AddSalesViewModel));
             AddProductPage = new NavigateCommand(navService, nameof(AddProductViewModel));
@@ -32,6 +41,8 @@ namespace MemberManagementSystem.ViewModel
             ViewSalesPage = new NavigateCommand(navService, nameof(ViewSalesViewModel));
             ViewProductPage = new NavigateCommand(navService, nameof(ViewProductViewModel));
             ViewUserPage = new NavigateCommand(navService, nameof(ViewUserViewModel));
+
+            OpenUserManual = new OpenURLCommand(UserManualURL);
         }
     }
 }
